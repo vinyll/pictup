75ba4cf61f4f1160ec4987c79c3e60437c8c5b5d
Vincent Agnano <vincent.agnano@scopyleft.fr>
2016-01-31 18:03:36 +0100

    removed render() and added replace option

--- src/pictup.js
+++ src/pictup.js
@@ -1,60 +1,82 @@
 (function(module) {
+  'use strict';
 
-  var imageContainer;
+  class Pictup {
 
-  function render(src) {
-    var MAX_WIDTH = imageContainer.maxwidth || imageContainer.width;
-    var MAX_HEIGHT = imageContainer.maxheight || imageContainer.height;
-    var image = new Image();
-    image.onload = function() {
-      var canvas = document.createElement('canvas');
-      canvas.width = MAX_WIDTH;
-      canvas.height = MAX_HEIGHT;
-      document.body.appendChild(canvas);
-      if (image.height > MAX_HEIGHT) {
-        image.width *= MAX_HEIGHT / image.height;
-        image.height = MAX_HEIGHT;
-      }
-      var ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      // canvas.width = image.width;
-      // canvas.height = image.height;
-      ctx.drawImage(image, 0, 0, image.width, image.height);
-    };
-    image.src = src;
-    return src;
-  }
+    constructor(target, options) {
+      this.target = target;
+      let defaultOptions = {
+        // Resize this image
+        resize: true,
+        // Max width and height for the result image
+        maxwidth: 0,
+        maxheight: 0,
+        // Replace the current image src with the result
+        replace: true,
+      };
+      this.options = Object.assign({}, defaultOptions, options);
 
-  function loadImage(src) {
-    //	Prevent any non-image file type from being read.
-    if (!src.type.match(/image.*/)) {
-      console.error('The dropped file is not an image: ', src.type);
-      return;
+      target.addEventListener('dragover', (e) => e.preventDefault(), true);
+
+      target.addEventListener('drop', (e) => {
+        e.preventDefault();
+        this.loadImage(e.dataTransfer.files[0]);
+      }, true);
     }
 
-    //	Create our FileReader and run the results through the render function.
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var content = render(e.target.result);
-      console.log('content', content);
-    };
+    loadImage(file) {
+      //	Prevent any non-image file type from being read.
+      if (!file.type.match(/image.*/)) {
+        throw new Error(`The dropped file is not an image but a ${src.type}`);
+      }
 
-    reader.readAsDataURL(src);
-    imageContainer.dispatchEvent(new CustomEvent('change', {detail: {
-      file: src,
-    }}));
+      let reader = new FileReader();
+      reader.addEventListener('load', (e) => {
+        var content = e.target.result;
+        if (this.options.resize) {
+          this.resize(content, this.options.maxwidth, this.options.maxheight);
+        }
+        else {
+          this.ready(content);
+        }
+      });
+      reader.readAsDataURL(file);
+    }
+
+    resize(content, maxwidth, maxheight) {
+      if (!maxwidth) {
+        let imageSrc = this.target;
+        maxwidth = imageSrc.style.maxwidth || imageSrc.width;
+        maxheight = imageSrc.style.maxheight || imageSrc.height;
+      }
+      var image = new Image();
+      image.onload = () => {
+        var canvas = document.createElement('canvas');
+        if (image.height > maxheight) {
+          image.width *= maxheight / image.height;
+          image.height = maxheight;
+        }
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+
+        this.ready(canvas.toDataURL());
+      };
+      image.src = content;
+      return content;
+    }
+
+    ready(content) {
+      if (this.options.replace) {
+        this.target.src = content;
+      }
+      this.target.dispatchEvent(new CustomEvent('ready', { detail: content }));
+    }
   }
 
-  var target = document.querySelector('[data-anypic]');
-  target.addEventListener('dragover', function(e) {
-    e.preventDefault();
-  }, true);
-
-  target.addEventListener('drop', function(e) {
-    e.preventDefault();
-    imageContainer = e.target;
-    console.log(imageContainer)
-    loadImage(e.dataTransfer.files[0]);
-  }, true);
+  module.Pictup = Pictup;
 
 })(window);
